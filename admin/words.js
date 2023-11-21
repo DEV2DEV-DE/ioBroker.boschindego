f62bc415747b8df6f5c9cf289a881a9a0ac90cd3
DEV2DEV_DE <github@dev2dev.de>
2023-11-21 14:11:13 +0100

    Text in settings page translated

--- admin/words.js
+++ admin/words.js
@@ -1,31 +1,85 @@
 /* eslint no-unused-vars: off */
 /* eslint no-global-assign: off */
 /* global systemDictionary */
-'use strict';
+"use strict";
 
 systemDictionary = {
-	'boschindego adapter settings': {
-		'en': 'Adapter settings for boschindego',
-		'de': 'Adaptereinstellungen für boschindego',
+		"boschindego adapter settings": {
+		"en": "Adapter settings for boschindego",
+		"de": "Adaptereinstellungen für boschindego",
+		"ru": "Параметры адаптера для boschindego",
+		"pt": "Configurações do adaptador para boschindego",
+		"nl": "Vertaling:",
+		"fr": "Paramètres d'adaptateur pour boschindego",
+		"it": "Impostazioni adattatori per boschindego",
+		"es": "Ajustes del adaptador para boschindego",
+		"pl": "Adapter ustawienia dla boschindego",
+		"uk": "Параметри адаптера для босчиндего",
+		"zh-cn": "诱惑环境"
 	},
-	'code': {
-		'en': 'Authentication code',
-		'de': 'Authentifizierungscode',
+	"code": {
+		"en": "Authentication code",
+		"de": "Authentifizierungscode",
+		"ru": "Код аутентификации",
+		"pt": "Código de autenticação",
+		"nl": "Authenticatie code",
+		"fr": "Code d ' authentification",
+		"it": "Codice di autenticazione",
+		"es": "Código de autenticación",
+		"pl": "Kod authetyczny",
+		"uk": "Поштовий індекс",
+		"zh-cn": "治疗守则"
 	},
-	'serial': {
-		'en': 'Serial number',
-		'de': 'Seriennummer',
+	"serial": {
+		"en": "Serial number",
+		"de": "Seriennummer",
+		"ru": "Серийный номер",
+		"pt": "Número de série",
+		"nl": "Serienummer",
+		"fr": "Numéro de série",
+		"it": "Numero di serie",
+		"es": "Número de serie",
+		"pl": "Numer szeregowy",
+		"uk": "Серійний номер",
+		"zh-cn": "数量"
 	},
 	"deepSleepAtNight": {
 		"en": "Don't wake up mower during night time (22h - 8h) for status checks",
 		"de": "Mäher während der Nachtzeit (22h - 8h) nicht für Statusprüfungen aufwecken",
+		"ru": "Не просыпайтесь во время ночного времени (22h - 8h) для проверки статуса",
+		"pt": "Não acorde o cortador durante a noite (22h - 8h) para verificações de status",
+		"nl": "Wordt niet wakker tijdens de nacht",
+		"fr": "Ne réveillez pas la tondeuse pendant la nuit (22h - 8h) pour vérifier le statut",
+		"it": "Non svegliare la falciatrice durante la notte (22h - 8h) per i controlli di stato",
+		"es": "No despiertes la cortadora durante la noche (22h - 8h) para cheques de estado",
+		"pl": "Don't wake up mower at night time (22h – 8h) for status checks (ang.)",
+		"uk": "Не прокидається косарка протягом нічного часу (22h - 8h) для перевірок стану",
+		"zh-cn": "在夜间(22h-8h)进行身份检查之后缴费"
 	},
 	"hint_code": {
 		"en": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
 		"de": "Um an den Authentifizierungscode zu gelangen, folge <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>diesem Link</a>.<br>Er leitet Dich auf die Anmeldeseite von SingleKey ID. Der Adapter erlangt dabei keine Kenntnis über Deinen Benutzernamen oder Kennwort!<br>Öffne vor der Eingabe Deiner Zugansdaten die Developer-Tools im Browser. Normalerweise geht das mit der Taste F12.<br>Nachdem Du Deine Zugansdaten eingegeben hast, wird der Authentifizierungscode im Fenster der Developer-Tools angezeigt.<br>Kopiere den dort hinterlegten Code und füge ihn hier ein.",
+		"ru": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"pt": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"nl": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"fr": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"it": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"es": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"pl": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"uk": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here.",
+		"zh-cn": "To obtain the authentication code, follow <a target='_blank' href='https://prodindego.b2clogin.com/prodindego.onmicrosoft.com/b2c_1a_signup_signin/oauth2/v2.0/authorize?redirect_uri=com.bosch.indegoconnect://login&client_id=65bb8c9d-1070-4fb4-aa95-853618acc876&response_type=code&scope=openid%20offline_access%20https://prodindego.onmicrosoft.com/indego-mobile-api/Indego.Mower.User'>this link</a>.<br>It will redirect you to the SingleKey ID login page. The adapter does not gain any knowledge of your username or password!<br>Open the developer tools in your browser before entering your access data. Usually, you can do this by pressing F12.<br>After you have entered your access data, the authentication code will be displayed in the developer tools window.<br>Copy the code provided there and paste it here."
 	},
 	"hint serial": {	
 		"en": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
 		"de": "Die Seriennummer finden Sie auf der Rückseite des Mähers oder in der App unter 'Einstellungen' -> 'Mäher' -> 'Info' -> 'Seriennummer'",
+		"ru": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"pt": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"nl": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"fr": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"it": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"es": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"pl": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"uk": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'",
+		"zh-cn": "You can find the serial number on the back of the mower or in the app under 'Settings' -> 'Mower' -> 'About' -> 'Serial number'"
 	}
 };
